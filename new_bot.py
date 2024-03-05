82a244dff7d8966838bad3eb2d3ef93b11c180c0
Kevgen <87700221+k3vg3n@users.noreply.github.com>
2024-03-05 19:52:10 +0300

    Update new_bot.py

--- new_bot.py
+++ new_bot.py
@@ -1,24 +1,31 @@
-# В разработке
+# В разработке!
 
 import telebot
 from telebot import types
+from datetime import datetime, timedelta
 
+current_time = datetime.now()
 
 users_data = []
 user_solves = []
 all_users = []
 
-bot = telebot.TeleBot("6736013664:AAEKl_kFj9QL55nS1X2iq_SZsuKbdL9tr74")
+bot = telebot.TeleBot("TOKEN")
+
+
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, "Привет, " + message.from_user.first_name + "!")
 
 
+def show_stats(message):
+    for user in users_data:
+            if user['user_id'] == message.from_user.id:
+                bot.send_message(message.chat.id, "Ваша статистика:\n" + "Всего солвов: " + str(len(user['solves'])))
+                break
 
-
-@bot.message_handler(func=lambda message: True)
-def handle_message(message):
+def add_solve(message):
     # Получаем текст входящего сообщения
     text = message.text
     # users_data.append({'user_id': message.from_user.id, 'solves': []})
@@ -27,7 +34,7 @@ def handle_message(message):
         task_flag = text.split(' ')[1]
         url = text.split(' ')[2]
     except:
-        pass
+        return
         
 
     for user in users_data:
@@ -40,15 +47,17 @@ def handle_message(message):
         if user['user_id'] == message.from_user.id:
             user['solves'].append({'task_name': task_name, 'task_flag': task_flag, 'url': url})
             break
-    
 
-    # response = 'Вы отправили: {}'.format(text)
-    # bot.send_message(message.chat.id, response)
 
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    if message.text == "!stats":
+        show_stats(message)
+    else:
+        add_solve(message)
     print(users_data)
 
 
 
-# Запускаем бота
 bot.polling()
 
